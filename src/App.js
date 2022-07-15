e2294cef54ff38a095c45464928a748bae34ab14
mabbas112 <mabbasctn112@gmail.com>
2022-07-15 20:42:55 +0500

    render items, cart functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Cart from './Components/Cart/Cart';
 import Header from './Components/Header/Header';
+import AvailableMeals from './Components/Meals/AvailableMeals';
 import MealsSummary from './Components/MealsSummary/MealsSummary';
+import { CartContextProvider } from './Components/store/CartContext';
 
 function App() {
+
+  const [toggleCart, setToggleCart] = useState(false);
+  const toggleCartHandler = () => {
+    setToggleCart(prevState => !prevState)
+  }
+
   return (
-    <React.Fragment>
-      <Header/>
-      <MealsSummary/>
-    </React.Fragment>
+    <CartContextProvider>
+      {toggleCart && <Cart onToggleCart={toggleCartHandler} />}
+      <Header onToggleCart={toggleCartHandler} />
+      <MealsSummary />
+      <AvailableMeals />
+    </CartContextProvider>
   );
 }
 
